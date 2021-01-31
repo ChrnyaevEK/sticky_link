502853d3658a77a504464b4c54d89cf07c2a2be2
cherniaev egor <chrnyaevek@gmail.com>
2021-01-31 19:48:03 +0100

    Web: header + footer Workspace: selector

--- templates/application/src/components/Workspace.vue
+++ templates/application/src/components/Workspace.vue
@@ -1,50 +1,43 @@
 <template>
-    <div class="container">
-        <div class="card">
-            <div class="card-body">
-                <h4 class="card-title"><LoremIpsum add="1s"></LoremIpsum></h4>
-                <div class="collapse" id="container-header-collapse">
-                    <div class="row">
-                        <div class="col-xs-12">
-                            <p class="card-text">
-                                <LoremIpsum add="1p"></LoremIpsum>
-                            </p>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 col-md-2">
+                <div class="font-weight-bold border-bottom p-3">Widgets</div>
+                <div id="workspace-widget-selector" role="tablist" aria-multiselectable="true">
+                    <div class="card border-0">
+                        <div class="card-header p-2 bg-light" role="tab" id="section-1-header">
+                            <a data-toggle="collapse" data-parent="#workspace-widget-selector" href="#section-1-body" aria-expanded="true" aria-controls="section-1-body">
+                                Text widgets
+                            </a>
+                        </div>
+                        <div id="section-1-body" class="collapse in" role="tabpanel" aria-labelledby="section-1-header">
+                            <div class="card-body p-2 row">
+                                <div class="col-10">Simple text card</div>
+                                <div class="col-2"><i class="fas fa-plus"></i></div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="text-right">
-                    <a
-                        class="btn btn-primary float-end"
-                        data-toggle="collapse"
-                        href="#container-header-collapse"
-                        aria-expanded="false"
-                        aria-controls="contentId"
-                    >
-                        Expand
-                    </a>
-                </div>
             </div>
-            <!-- Cards here -->
+            <div class="col-12 col-md-10" id="workspace-widget-list"></div>
         </div>
     </div>
 </template>
 
 <script>
-// import SimpleTextCard from "./Cards/SimpleTextCard";
-import LoremIpsum from "vue-lorem-ipsum";
-export default {
-    name: "Workspace",
-    data: function(){
-        return {
-            meta: {},
-            entries: [
-
-            ]
-        }
-    },
-    components: {
-        LoremIpsum,
-        // SimpleTextCard,
-    },
-};
+    // import SimpleTextCard from "./Cards/SimpleTextCard";
+    // import LoremIpsum from "vue-lorem-ipsum";
+    export default {
+        name: "Workspace",
+        data: function() {
+            return {
+                meta: {},
+                entries: [],
+            };
+        },
+        components: {
+            // LoremIpsum,
+            // SimpleTextCard,
+        },
+    };
 </script>
