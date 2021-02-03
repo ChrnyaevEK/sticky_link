e3c5ce80dd53ced1c3b556c553d0b9b10c7b1706
cherniaev egor <chrnyaevek@gmail.com>
2021-02-04 00:03:18 +0100

    Static files... and other changes

---
+++ templates/application/src/components/Widgets/View/SimpleText.vue
@@ -0,0 +1,30 @@
+<template id="simple-text-widget">
+    <WidgetBase v-bind="{ ...$props, ...$attrs }">
+        <template slot="content">
+            <div class="d-flex flex-column h-100">
+                <span class="flex-grow-1 text-truncate text-wrap text-break">
+                    {{ widget.text_content }}
+                </span>
+                <span class="text-right text-secondary">{{ widget.text_content ? widget.text_content.length : 0 }}/{{ widget.max_length }}</span>
+            </div>
+        </template>
+    </WidgetBase>
+</template>
+
+<script>
+    import WidgetBase from "../WidgetBase.vue";
+    // import API from "../../../common.js";
+    export default {
+        name: "SimpleText",
+        template: "#simple-text-widget",
+        props: {
+            widget: {
+                type: Object,
+                required: true,
+            },
+        },
+        components: {
+            WidgetBase,
+        },
+    };
+</script>
